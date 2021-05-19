b12c69decf1ffbcec59c62c21d6c444fd3e5029c
insane4l <43768644+insane4l@users.noreply.github.com>
2021-05-19 11:34:30 +0300

    New counter project, lesson 69 Redux basic principles(actions, store, reducers)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DEC':
+      return state - 1;
+    case 'RNDM':
+      return state + action.rndmValue;
+    default:
+      return state;
+  }
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducer);
+
+
+const inc = () => ({type: 'INC'}); //"action creator" function
+const dec = () => ({type: 'DEC'});
+const rndm = (rndmValue) => ({type: 'RNDM', rndmValue});
+
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch(inc());
+})
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec());
+})
+document.getElementById('rndm').addEventListener('click', () => {
+  const rndmValue = Math.floor(Math.random() * 10);
+  store.dispatch(rndm(rndmValue));
+})
+
+
+const updateCounter = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(updateCounter);
